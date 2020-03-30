3ec143fd49bd670bc8727010b902dbbe2c13e0cc
Allan Smiff <allandifere@gmail.com>
2020-03-29 23:57:14 -0300

    arrumando link do index

--- portapp/urls.py
+++ portapp/urls.py
@@ -4,6 +4,7 @@ from .views import index, about, contact, services
 urlpatterns = [
 
     path('', index, name='inicio'),
+    path('index.html', index),
     path('about.html', about, name='sobre'),
     path('contact.html', contact, name='contato'),
     path('services.html', services, name='serviços'),
